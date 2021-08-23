74ea8b618c755b8463dcc4566f4f61b8754d1108
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2021-08-23 19:06:23 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,36 +1,16 @@
-import numpy as np
-items = int(input("Enter no of items : "))
-capacity = int(input("Enter Capacity : "))
-pi =[0]+ list(map(int,input("Enter Profits : ").split()))
-w = [0]+list(map(int,input("Enter weights : ").split()))
-item = list(i for i in range(items+1))
-p = np.zeros([items+1, capacity+1],dtype=int)
-for i in range(1,items+1):
-    for j in range(1,capacity+1):
-        l = list()
-        a = p[i-1,j]
-        if(j<w[i]):
-            b = -1
-        else:
-            b = p[i-1,j-w[i]]+pi[i]
-        p[i,j] += int(max(a,b))
-m = p[items,capacity]
-backtrack = [0]*(items+1)
-for i in range(items,0,-1):
-    if m in p[i] and m in p[i-1] :
-        continue
-    else :
-        m -= pi[i]
-        backtrack[i] = 1
-print("maximum profit that can be obtained is ",p[items,capacity])
-print("Items are : ")
-print([i for i in range(len(backtrack)) if backtrack[i]==1 ])
+def knapSack(c, wt, val, n):
+    if n == 0 or c == 0:
+        return 0
+    if (wt[n-1] > c):
+        return knapSack(c, wt, val, n-1)
+    else:
+        return max(
+            val[n-1] + knapSack(c-wt[n-1], wt, val, n-1),
+            knapSack(c, wt, val, n-1))
 
-
-'''
-sample input
-7
-15
-10 5 15 7 6 18 3
-2 3 5 7 1 4 1
-'''
+val = list(map(int,input("Enter the values : ").split()))
+wt = list(map(int,input("Enter the weights : ").split()))
+wt.sort()
+c = int(input("enter the maximum capacity : "))
+n = len(val)
+print( knapSack(c, wt, val, n))
