b90044537e525ee76ecab1ec4b8f2e19664cd1ec
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2021-08-25 14:53:27 +0530

    addition

--- 1.2.py
+++ 1.2.py
@@ -1,33 +1,19 @@
-def inp(row,col):
-    a = []
-    for i in range(row):
-        b = []
-        for j in range(col):
-            b.append(int(input()))
-        a.append(b)
-    return a
-def mul(x,y,row,col,n):
-    a = []
-    for i in range(row):
-        b = []
-        for j in range(col):
-            h = 0
-            for k in range(n):
-               h += x[i][k]*y[k][j]
-            b.append(h)
-        a.append(b)
-    return a
-r1 = int(input("enter row size of 1st matrix"))
-c1 = int(input("enter column size of 1st matrix"))
-print("enter the elements")
-a = inp(r1,c1)
-r2 = int(input("enter row size of 2nd matrix"))
-c2 = int(input("enter column size of 2nd matrix"))
-print("enter the elements")
-b = inp(r2,c2)
-if(r2==c1):
-    c = mul(a,b,r1,c2,c1)
-    print("After Multiplication")
-    print(c)
-else :
-    print("Addition not Possible")
+n = int(input())
+m = int(input())
+m1 = [[int(input()) for j in range(m) ] for i in range(n)]
+
+for i in range(m):
+    for j in range(n):
+        print(m1[i][j], end=" ")
+    print("")
+m2 = [[int(input()) for j in range(m) ] for i in range(n)]
+
+for i in range(m):
+    for j in range(n):
+        print(m2[i][j], end=" ")
+    print("")
+
+for i in range(m):
+    for j in range(n):
+        print(m1[i][j]+m2[i][j], end=" ")
+    print("")
