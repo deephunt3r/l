0e08631228ada700660a32b7227ee88a2dc59e50
lab1923 <89344991+lab1923@users.noreply.github.com>
2021-08-28 11:53:56 +0530

    Update 1a.py

--- pos/1a.py
+++ pos/1a.py
@@ -28,3 +28,28 @@ for i in range(0,len(p)):
     print("  ",p[i],"   \t",at_d[i],"   \t",bt[i],"\t\t",ct[i],"\t\t",tat[i],"\t\t",wt[i],"\t\t")
 print("Average WaitingTime=",sum(wt)/len(wt))
 print("Average TurnAroundTime=",sum(tat)/len(tat))
+
+
+#n = int(input())
+# at = list(map(int,input().split()))
+# bt = list(map(int,input().split()))
+# table = []
+# for i in range(n):
+#     table.append([i+1,at[i],bt[i]])
+# table = sorted(table, key = lambda x:x[1])
+# CT = 0
+# c = 0
+# while(c<n):
+#     if(table[c][1]<=CT):
+#         CT += table[c][2]
+#         # print(table[c])
+#         table[c].append(CT)
+#         table[c].append(CT-table[c][1])
+#         table[c].append(CT-table[c][1]-table[c][2])
+#         c += 1
+#     else :
+#         CT += 1
+# table = sorted(table,key = lambda x:x[0])
+# for i in table:
+#     print(i)
+# print(CT)
