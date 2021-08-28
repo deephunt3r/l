692499b5dfdf05506a7a9b1cab10c4ec27e42790
kituu02 <68700179+kituu02@users.noreply.github.com>
2021-08-28 09:51:52 +0530

    Update 1d.py

--- pos/1d.py
+++ pos/1d.py
@@ -46,3 +46,46 @@ for i in range(0,len(pt)):
 print("Average WaitingTime=",sum(wt)/len(wt))
 print("Average TurnAroundTime=",sum(tat)/len(tat))
 
+# n = int(input())
+# at = list(map(int,input().split()))
+# bt = list(map(int,input().split()))
+# pr = list(map(int,input().split()))
+# table = []
+# for i in range(n):
+#     table.append([0,i,at[i],bt[i],pr[i]])
+# result = []
+# c = CT = 0
+# while(c<n):
+#     min_list = []
+#     for j in range(n):
+#         if(table[j][2]<=CT and table[j][0]==0):
+#             min_list.append(table[j])
+#     if(len(min_list)==0):
+#         CT += 1
+#         continue
+#     m = min_list[0][4]
+#     index = 0
+#     for i in range(len(min_list)):
+#         if(m>min_list[i][4] ):
+#             m = min_list[i][4]
+#             index = i
+#     CT += min_list[index][3]
+#     bt1 = min_list[index][3]
+#     at1 = min_list[index][2]
+#     pr1 =  min_list[index][4]
+#     p = min_list[index][1]+1
+#     ct = CT
+#     tat = ct-at1
+#     wt = ct-bt1
+#     result.append([p,at1,bt1,pr1,ct,tat,wt])
+#     for i in range(n):
+#         if(m==table[i][4] and table[i][0]==0):
+#             table[i][0] = 1
+#     c += 1
+# print("Ganttchart : ")
+# for i in result:
+#     print("p"+str(i[0]),end = " ")
+# print()
+# result = sorted(result,key = lambda x: x[0])
+# for i in result:
+#     print(i)
